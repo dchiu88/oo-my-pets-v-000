d762def228ec1d297e1dde60d80e499cd19a8420
David Chiu <davidwchiu@gmail.com>
2018-06-12 09:38:02 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -64,6 +64,6 @@ class Owner
   end
   
   def list_pets
-    "I have #{pets[:fishes.size] fish, #{pets[ "
+    "I have #{pets[:fishes.size]} fish, #{pets[:dogs.size]} dogs, #{pets[:cats.size]} cats. "
   
 end
